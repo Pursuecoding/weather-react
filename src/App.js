f9e29778627015bedc43634c086f2eea1dfae38d
Pursuecoding <131742876+Pursuecoding@users.noreply.github.com>
2023-08-19 19:04:50 -0400

    Added {" "}

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Weathter App</h1>
         <p>
-          This app was created by Oksana Yanko and it is 
+          This app was created by Oksana Yanko and it is{" "}
         <a
           className="App-link"
           href="https://github.com"
