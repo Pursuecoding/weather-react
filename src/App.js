11bb4b61271dac768af0204e037fd8a50915880d
Pursuecoding <131742876+Pursuecoding@users.noreply.github.com>
2023-08-21 14:33:44 -0400

    Added links

--- src/App.js
+++ src/App.js
@@ -8,14 +8,32 @@ function App() {
       <header className="App-header">
         <Weather defaultCity="Gainesville" />
         <p>
-          This app was created by Oksana Yanko and it is{" "}
+          This app was created by {" "}
+          <a
+          className="App-link"
+          href="https://oksana-yanko-portfolio.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Oksana Yanko.
+        </a>
+           {" "}It is{" "}
         <a
           className="App-link"
           href="https://github.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          open-sourced
+          open-sourced on GitHub
+        </a>
+        {" "}and hosted on{" "}
+        <a
+          className="App-link"
+          href="https://jocular-stroopwafel-aeaef2.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
         </a>
         </p>
       </header>
