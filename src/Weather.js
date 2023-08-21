923aef311250e6b8737a8786ce798e8789f66d33
Pursuecoding <131742876+Pursuecoding@users.noreply.github.com>
2023-08-21 14:24:39 -0400

    Search Engine integration

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,7 @@ import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
+    const [city, setCity] = useState(props.defaultCity)
     const [weatherData, setWeatherData] = useState({ready: false});
 
     function handleResponse(response) {
@@ -21,13 +22,33 @@ export default function Weather(props) {
         })
     }
 
+    function search() {
+        const apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
+    }
+
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-9">
-                    <input type="search" placeholder="Enter a city" className="form-control" />
+                    <input 
+                    type="search" 
+                    placeholder="Enter a city" 
+                    className="form-control"
+                    autoFocus="on"
+                    onChange={handleCityChange} />
                     </div>
                     <div className="col-3">
                     <input type="submit" value="Search" className="btn btn-primary" />
@@ -60,9 +81,7 @@ export default function Weather(props) {
             </div>
         );
     } else {
-    let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
-    axios.get(apiUrl).then(handleResponse);
+    search();
 
     return "Loading...";
     }
